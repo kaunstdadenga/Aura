b0c117ab9ce86fb6f9598c8e89af2b8a4a14f1b1
kaunstdadenga <jan.brunner.at@gmail.com>
2023-09-21 14:29:14 +0200

    Fix The Base Character

---
+++ Source/Aura/Private/AAuraCharacterBase.cpp
@@ -0,0 +1,16 @@
+// Copyright kaunstdadenga
+
+
+#include "AAuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+
