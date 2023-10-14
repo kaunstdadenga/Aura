6f723fddc67df59faed96a3f7086f780a44e02f6
kaunstdadenga <jan.brunner.at@gmail.com>
2023-10-14 12:06:10 +0200

    GameplayTagsManager

---
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -0,0 +1,10 @@
+// Copyright kaunstdadenga
+
+
+#include "AuraGameplayTags.h"
+#include "GameplayTagsManager.h"
+
+FAuraGameplayTags FAuraGameplayTags::GameplayTags;
+void FAuraGameplayTags::InitializeNativeGameplayTags() {
+	UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.Armor"), FString("Reduces Damage taken, improves Block Change"));
+}
