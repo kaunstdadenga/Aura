f691808ca45babcf921ec28052cb41e72cf6517a
kaunstdadenga <jan.brunner.at@gmail.com>
2023-11-09 21:40:55 +0100

    Combat Target

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -60,6 +60,14 @@ void AAuraEnemy::UnHighlightActor() {
 	Weapon->SetRenderCustomDepth(false);
 }
 
+void AAuraEnemy::SetCombatTarget_Implementation(AActor* InCombatTarget) {
+	CombatTarget = InCombatTarget;
+}
+
+AActor* AAuraEnemy::GetCombatTarget_Implementation() const {
+	return CombatTarget;
+}
+
 int32 AAuraEnemy::GetPlayerLevel() {
 	Super::GetPlayerLevel();
 	return Level;
