8eada61181a4b50d7336a1365d8ae14bb1536be3
kaunstdadenga <jan.brunner.at@gmail.com>
2023-12-22 22:03:29 +0100

    Goblin Spear - Hurt and Death Sounds

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -8,6 +8,7 @@
 #include "AbilitySystem/AuraAbilitySystemComponent.h"
 #include "Aura/Aura.h"
 #include "Components/CapsuleComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 AAuraCharacterBase::AAuraCharacterBase() {
 	PrimaryActorTick.bCanEverTick = false;
@@ -37,6 +38,8 @@ void AAuraCharacterBase::Die() {
 }
 
 void AAuraCharacterBase::MulticastHandleDeath_Implementation() {
+	UGameplayStatics::PlaySoundAtLocation(this, DeathSound, GetActorLocation(), GetActorRotation());
+
 	Weapon->SetSimulatePhysics(true);
 	Weapon->SetEnableGravity(true);
 	Weapon->SetCollisionEnabled(ECollisionEnabled::PhysicsOnly);
