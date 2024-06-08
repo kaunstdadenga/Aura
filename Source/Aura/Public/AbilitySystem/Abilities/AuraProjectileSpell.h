2d19df475122393b05b1e39ff5e49f3298258810
kaunstdadenga <jan.brunner.at@gmail.com>
2024-06-08 22:54:17 +0200

    FireBolt Description

--- Source/Aura/Public/AbilitySystem/Abilities/AuraProjectileSpell.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraProjectileSpell.h
@@ -18,6 +18,10 @@ UCLASS()
 class AURA_API UAuraProjectileSpell : public UAuraDamageGameplayAbility {
 	GENERATED_BODY()
 
+public:
+	virtual FString GetDescription(int32 Level) override;
+	virtual FString GetNextLevelDescription(int32 Level) override;
+
 protected:
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
 	                             const FGameplayAbilityActivationInfo ActivationInfo,
@@ -29,4 +33,7 @@ protected:
 	UFUNCTION(BlueprintCallable, Category="Projectile")
 	void SpawnProjectile(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag,
 	                     bool bOverridePitch = false, float PitchOverride = 0.f);
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 NumProjectiles = 5;
 };
