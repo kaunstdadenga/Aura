adc97170525293487388c7cf95844c066ed9b6cc
kaunstdadenga <jan.brunner.at@gmail.com>
2024-06-16 11:59:13 +0200

    Handling Death Impulse

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -72,12 +72,12 @@ int32 AAuraEnemy::GetPlayerLevel_Implementation() {
 	return Level;
 }
 
-void AAuraEnemy::Die() {
+void AAuraEnemy::Die(const FVector& DeathImpulse) {
 	SetLifeSpan(LifeSpan);
 	if (AuraAIController) {
 		AuraAIController->GetBlackboardComponent()->SetValueAsBool("Dead", true);
 	}
-	Super::Die();
+	Super::Die(DeathImpulse);
 }
 
 void AAuraEnemy::BeginPlay() {
