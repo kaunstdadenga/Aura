64df9401a68b5824732cc9cc6797d693e0a1a9e5
kaunstdadenga <jan.brunner.at@gmail.com>
2024-06-19 22:00:55 +0200

    Electrocute Montage

--- Source/Aura/Private/AbilitySystem/Abilities/AuraBeamSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraBeamSpell.cpp
@@ -3,6 +3,8 @@
 
 #include "AbilitySystem/Abilities/AuraBeamSpell.h"
 
+#include "GameFramework/Character.h"
+
 void UAuraBeamSpell::StoreMouseDataInfo(const FHitResult& HitResult) {
 	if (HitResult.bBlockingHit) {
 		MouseHitLocation = HitResult.ImpactPoint;
@@ -13,8 +15,9 @@ void UAuraBeamSpell::StoreMouseDataInfo(const FHitResult& HitResult) {
 	}
 }
 
-void UAuraBeamSpell::StoreOwnerPlayerController() {
+void UAuraBeamSpell::StoreOwnerVariables() {
 	if (CurrentActorInfo) {
 		OwnerPlayerController = CurrentActorInfo->PlayerController.Get();
+		OwnerCharacter = Cast<ACharacter>(CurrentActorInfo->AvatarActor);
 	}
 }
